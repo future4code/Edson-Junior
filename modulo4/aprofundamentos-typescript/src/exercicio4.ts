b7169b3fde53b2c33d5ef90e8d2faf15668da926
edmagnini <edson_magnini@hotmail.com>
2022-01-20 16:47:20 -0300

    aprofundamentos typescript

---
+++ modulo4/aprofundamentos-typescript/src/exercicio4.ts
@@ -0,0 +1,27 @@
+type pokemon = {
+    name: string,
+    types: string,
+    healthPoints: number
+}
+
+const pokemon1: pokemon = {
+    name: "Charmander",
+    types: "Fire",
+    healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+    name: "Bulbasaur",
+    types: "Grass/Poison",
+    healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+    name: "Squirtle",
+    types: "Water",
+    healthPoints: 35
+}
+
+// b) instalação global do typescript e executando o comando para a pasta direcionando-o para o arquivo correto no src
+
+// c) Sim, é possivel configurando o tsconfig para verificar a pasta rootDir e outDir
