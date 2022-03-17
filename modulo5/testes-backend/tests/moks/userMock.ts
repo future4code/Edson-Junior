dd24bb0874b334dd9792326beaa6df22125d673d
Edson Magnini <edson_magnini@hotmail.com>
2022-03-16 21:31:14 -0300

    entrega de exercícios de testes

---
+++ modulo5/testes-backend/tests/moks/userMock.ts
@@ -0,0 +1,23 @@
+import { User, USER_ROLES } from "../../src/model/User";
+
+export const userMock = new User(
+    "id_mockado",
+    "astrodev",
+    "astrodev@gmail.com",
+    "astrodev123",
+    USER_ROLES.ADMIN
+)
+export const userMock2 = new User(
+    "id_mockado_2",
+    "astrodev2",
+    "astrodev2@gmail.com",
+    "astrodev123",
+    USER_ROLES.NORMAL
+)
+export const userMock3 = new User(
+    "id_mock_admin",
+    "astrodev",
+    "astrodev@gmail.com",
+    "astrodev123",
+    USER_ROLES.ADMIN
+)
